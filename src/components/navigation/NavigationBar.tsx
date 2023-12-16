b18a14259ee15dec363f371a7f99cf4cb1e4686b
Michał Bebłociński <94801124+Michalbeblocinski@users.noreply.github.com>
2023-12-16 20:54:08 +0100

    Updated: Finish second section view

--- src/components/navigation/NavigationBar.tsx
+++ src/components/navigation/NavigationBar.tsx
@@ -2,23 +2,25 @@ import React from 'react';
 import './NavigationBar.scss';
 import logo from '../../../assets/images/logo-bookmark.svg';
 import HamburgerMenu from '@src/components/navigation/hamburger-menu/hamburgerMenu';
+import { Link } from 'react-router-dom';
 
 export const NavigationBar: React.FC = () => {
   return (
     <div className='navigation-bar'>
-      <a href={'#'}>
+      <p>
         <img className={'navigation-bar__logo'} alt={'logo'} src={logo} />
-      </a>
+      </p>
+
       <div className={'navigation-bar__menu'}>
         <ul className={'navigation-bar__menu-list'}>
           <li className={'navigation-bar__menu-list-element'}>
-            <a href={'#'}>FEATURES</a>
+            <p>FEATURES</p>
           </li>
           <li className={'navigation-bar__menu-list-element'}>
-            <a href={'#'}>PRICING</a>
+            <p>PRICING</p>{' '}
           </li>
           <li className={'navigation-bar__menu-list-element'}>
-            <a href={'#'}>CONTACT</a>
+            <p>CONTACT</p>
           </li>
           <li className={'navigation-bar__menu-list-element'}>
             <button className={'navigation-bar__menu-list-element-button'}>
