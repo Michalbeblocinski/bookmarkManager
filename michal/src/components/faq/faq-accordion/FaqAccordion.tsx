e0333c5f02dfcdc112c39d445d8bc3fb910e5110
Michał Bebłociński <94801124+Michalbeblocinski@users.noreply.github.com>
2023-12-18 18:38:24 +0100

    Created: Own accordin. Delete mui

--- michal/src/components/faq/faq-accordion/FaqAccordion.tsx
+++ michal/src/components/faq/faq-accordion/FaqAccordion.tsx
@@ -1,89 +1,61 @@
 import * as React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import './FaqAccordion.scss';
+import { useState } from "react";
+import up from "@assets/icons/up.svg";
+import down from "@assets/icons/down.svg";
+
 
 export const FaqAccordion: React.FC = () => {
+  const [accordionHeaders, setAccordionHeaders] = useState([
+    {
+      title: 'What is Bookmark?',
+      textContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+      isExpanded: false,
+    },
+    {
+      title: 'How can I request a new browser?',
+      textContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+      isExpanded: false,
+    },
+    {
+      title: 'Is there a mobile app?',
+      textContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+      isExpanded: false,
+    },
+    {
+      title: 'What about other Chromium browsers?',
+      textContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+      isExpanded: false,
+    },
+  ]);
+
+  const toggleAccordionPanel = (index: any) => {
+    console.log("Accordion panel clicked:", index);
+    const currentPanel = accordionHeaders[index];
+    currentPanel.isExpanded = !currentPanel.isExpanded;
+    setAccordionHeaders([...accordionHeaders]);
+  };
+
   return (
-    <div className='landing-page__faq-accordions'>
-      <Accordion className={'landing-page__faq-accordions-accordion'}>
-        <AccordionSummary
-          className='landing-page__faq-accordions-accordion-summary'
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls='panel1a-content'
-          id='panel1a-header'
+    <div className="landing-page__faq-accordions">
+      {accordionHeaders.map((header, index) => (
+        <div
+          className="landing-page__faq-accordions-accordion"
+          key={index}
+          onClick={() => toggleAccordionPanel(index)}
         >
-          <Typography className='landing-page__faq-accordions-accordion-topography'>
-            What is Bookmark?
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails className='landing-page__faq-accordions-accordion-details'>
-          <Typography className='landing-page__faq-accordions-accordion-details-text'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion className={'landing-page__faq-accordions-accordion'}>
-        <AccordionSummary
-          className='landing-page__faq-accordions-accordion-summary'
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls='panel1a-content'
-          id='panel1a-header'
-        >
-          <Typography className='landing-page__faq-accordions-accordion-topography'>
-            How can I request a new browser?
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails className='landing-page__faq-accordions-accordion-details'>
-          <Typography className='landing-page__faq-accordions-accordion-details-text'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion className={'landing-page__faq-accordions-accordion'}>
-        <AccordionSummary
-          className='landing-page__faq-accordions-accordion-summary'
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls='panel1a-content'
-          id='panel1a-header'
-        >
-          <Typography className='landing-page__faq-accordions-accordion-topography'>
-            Is there a mobile app?
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails className='landing-page__faq-accordions-accordion-details'>
-          <Typography className='landing-page__faq-accordions-accordion-details-text'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion className={'landing-page__faq-accordions-accordion'}>
-        <AccordionSummary
-          className='landing-page__faq-accordions-accordion-summary'
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls='panel1a-content'
-          id='panel1a-header'
-        >
-          <Typography className='landing-page__faq-accordions-accordion-topography'>
-            What about other Chromium browsers?
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails className='landing-page__faq-accordions-accordion-details'>
-          <Typography className='landing-page__faq-accordions-accordion-details-text'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+          <h3 className="landing-page__faq-accordions-accordion-topography">
+            {header.title}
+            {header.isExpanded ?  <img className={'landing-page__faq-accordions-accordion-topography-arrow'} alt={'arrow'} src={up} /> :  <img className={'landing-page__faq-accordions-accordion-topography-arrow'} alt={'arrow'} src={down} />}
+          </h3>
+          {header.isExpanded && (
+            <p className="landing-page__faq-accordions-accordion-details-text">
+              {header.textContent}
+            </p>
+          )}
+        </div>
+      ))}
     </div>
   );
 };
+
